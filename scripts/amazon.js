dc98b582dcdcc1655abd2d134f885b4c759d11dc
Kuldeep Singh <kuldeepsingh050600@gmail.com>
2024-01-05 02:10:13 +0530

    Cart Button done

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -45,16 +45,36 @@ products.forEach((product) =>{
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary">
+          <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id="${product.id}">
             Add to Cart
           </button>
         </div>
         
   `;
     
-})
+});
 
-console.log(productHTML);
+document.querySelector('.js-products-grid').innerHTML=productHTML;
 
-document.querySelector('.js-products-grid').
-innerHTML=productHTML;
+document.querySelectorAll('.js-add-to-cart')
+  .forEach((button) => {
+    button.addEventListener('click', () => {
+      const productId = button.dataset.productId;
+      let matchingItem;
+      cart.forEach((item)=>{
+        if(productId === item.productId){
+          matchingItem=item;
+        }
+      });
+      if(matchingItem){
+        matchingItem.Quantity++;
+      }else{
+        cart.push({
+        productId : productId,
+        Quantity : 1
+      });
+      }
+      
+      console.log(cart);
+    });
+  });
