9ef54460a1d0ffc90a23551efd38c8a8908edfd7
Kuldeep Singh <kuldeepsingh050600@gmail.com>
2024-01-06 03:03:42 +0530

    Save data and genrate HTML for Delivery Order

--- data/cart.js
+++ data/cart.js
@@ -2,10 +2,12 @@ export let cart = JSON.parse(localStorage.getItem('cart'));
 if (!cart){
   cart = [{
     productId : 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-    Quantity : 2
+    Quantity : 2,
+    deliveryOptionsId : '1'
     },{
       productId : '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-      Quantity : 1
+      Quantity : 1,
+      deliveryOptionsId : '2'
     }];
 }
 
@@ -24,7 +26,8 @@ let matchingItem;
     }else{
       cart.push({
       productId : productId,
-      Quantity : 1
+      Quantity : 1,
+      deliveryOptionsId : '1'
     });
     }
     saveToStorage();
